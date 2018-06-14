863d3475b175b3743e7ec0813430f87f795fe964
michelle <hwrng2@gmail.com>
2018-06-15 01:25:01 +0900

    FAST CAMPUS 파이썬&장고 웹 프로그래밍 day20: Django Framework 기본 설정

---
+++ day20/django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hwrng2/Documents/playspace/fastcampus-python-django/day20/django-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
