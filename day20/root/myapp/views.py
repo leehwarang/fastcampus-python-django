ef7fdb017a2be800cac5988180c5de47bf4f1f92
michelle <hwrng2@gmail.com>
2018-06-25 22:16:52 +0900

    21일차 view

--- day20/root/myapp/views.py
+++ day20/root/myapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import datetime
+
+from django.shortcuts import render, HttpResponse
+
+def say_hi(request):
+    now_time = str(datetime.datetime.now())
+    name = request.GET.get('name')
+
+    if not name:
+        name = '손님'
+    return HttpResponse(
+        "<h1>Hi {}</h1>".format(name)+ '<br/><br/>' + now_time)
 
 # Create your views here.
