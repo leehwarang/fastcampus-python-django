bc6a601862fba0d6a9ff919c9d9b6fd268339548
leehwarang <hwrng2@gmail.com>
2018-08-14 00:21:52 +0900

    코드 복습 및 주석 추가

--- day20/reviewsite/movie/views.py
+++ day20/reviewsite/movie/views.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup as bs
 from .models import SearchResult, Movie
 # Create your views here.
 
-def search_movie(request):
+def search_movie(request): #여기서 받은 request는 url에서 받은 것
     #크롤링해서 결과값을 찾고, 보여주는 곳
-    if request.method == "POST":
+    if request.method == "POST": #받은게 있다면 크롤링해서 model에 저장
         keyword = request.POST['keyword']
         i = 1
         search_result = SearchResult.objects.create(
@@ -33,7 +33,8 @@ def search_movie(request):
         
 
     #print(request.POST['keyword'])
-    return render(request, 'search.html')
+    return render(request, 'search.html') #넘겨 받았던, 아니던, search.html 호출 
+                                        #현재는 search.html에 넘겨줄 게 없기 때문에 매개 변수가 없는 것. 
 
 
 
