d87e857ca7faeee2fba3ae9c59ed8590c4537343
leehwarang <hwrng2@gmail.com>
2018-08-21 00:35:00 +0900

    28~29(1) 복습하면서 생긴 수정

--- day20/reviewsite/movie/urls.py
+++ day20/reviewsite/movie/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import search_movie
 
 urlpatterns = [
-    path('', search_movie, name="search")
+    path('', search_movie, name="search"), #같은 url이지만 값을 받지 않는 경우
+    path('<str:keyword>', search_movie, name="search-result"),#같은 url이지만 값을 받는 경우
 ]
--- day20/reviewsite/movie/views.py
+++ day20/reviewsite/movie/views.py
@@ -1,20 +1,27 @@
-from django.shortcuts import render #, HttpResponse
-
+from django.shortcuts import render, redirect #, HttpResponse
+from django.utils import timezone 
 import requests
 from bs4 import BeautifulSoup as bs
 
 from .models import SearchResult, Movie
 # Create your views here.
 
-def search_movie(request): #여기서 받은 request는 url에서 받은 것
+def search_movie(request, keyword=None): #여기서 받은 request는 url에서 받은 것
     #크롤링해서 결과값을 찾고, 보여주는 곳
     if request.method == "POST": #받은게 있다면 크롤링해서 model에 저장
         keyword = request.POST['keyword']
+        results = SearchResult.objects.filter(keyword=keyword).order_by('-search_date') #가장 최근에 검색한 것 부터 보여주기 위해서 search_date의 역순 정렬
+        if results: #검색한 데이터가 있고
+            recent = results[0] #가장 최근에 검색한 데이터
+            if recent.search_date - timezone.now() < timezone.timedelta(days = 1): #검색한지 하루가 안된 경우
+                print("검색한지 하루가 지나지 않았습니다.")
+                return redirect('search-result', keyword=keyword)
         i = 1
         search_result = SearchResult.objects.create(
             keyword = keyword
         )
         while True:
+            print("크롤링 시작")
             url = "http://www.cgv.co.kr/search/movie.aspx?query={}&page={}".format(keyword, i)
             response = requests.get(url)
             result = bs(response.text, 'html.parser')
@@ -30,6 +37,14 @@ def search_movie(request): #여기서 받은 request는 url에서 받은 것
                 i += 1
             except AttributeError:
                 break
+        return redirect('search-result', keyword=keyword)
+
+        #GET메소드 redirect된 경우, keyword가 있는 경우
+        if keyword:
+            result = SearchResult.objects.filter(keyword=keyword).order_by('-search_date')
+            return render(request, 'search.html', {'result': result[0]})
+
+        #GET메소드 ->그냥 html 파일로 렌더링 시키기 
         
 
     #print(request.POST['keyword'])
