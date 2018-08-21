1e3387149a33e20328f1a784aedf8f8e0fcd2339
leehwarang <hwrng2@gmail.com>
2018-08-22 01:08:23 +0900

    28-29회차(3): cgv 사이트 분석 프로그램을 장고에 적용 중

--- day20/reviewsite/movie/views.py
+++ day20/reviewsite/movie/views.py
@@ -8,8 +8,11 @@ from .models import SearchResult, Movie
 
 def search_movie(request, keyword=None): #여기서 받은 request는 url에서 받은 것
     #크롤링해서 결과값을 찾고, 보여주는 곳
+    print(keyword)
     if request.method == "POST": #받은게 있다면 크롤링해서 model에 저장
         keyword = request.POST['keyword']
+        if not keyword:
+            return redirect('search')
         results = SearchResult.objects.filter(keyword=keyword).order_by('-search_date') #가장 최근에 검색한 것 부터 보여주기 위해서 search_date의 역순 정렬
         if results: #검색한 데이터가 있고
             recent = results[0] #가장 최근에 검색한 데이터
@@ -39,14 +42,15 @@ def search_movie(request, keyword=None): #여기서 받은 request는 url에서 
                 break
         return redirect('search-result', keyword=keyword)
 
-        #GET메소드 redirect된 경우, keyword가 있는 경우
-        if keyword:
-            result = SearchResult.objects.filter(keyword=keyword).order_by('-search_date')
+    #GET메소드 redirect된 경우, keyword가 있는 경우
+    if keyword:
+        result = SearchResult.objects.filter(keyword=keyword).order_by('-search_date')
+        if result[0].movie_set.all():
             return render(request, 'search.html', {'result': result[0]})
+        else: 
+            return render(request, 'search.html', {'result': False})
 
-        #GET메소드 ->그냥 html 파일로 렌더링 시키기 
-        
-
+    #GET메소드 ->그냥 html 파일로 렌더링 시키기 
     #print(request.POST['keyword'])
     return render(request, 'search.html') #넘겨 받았던, 아니던, search.html 호출 
                                         #현재는 search.html에 넘겨줄 게 없기 때문에 매개 변수가 없는 것. 
